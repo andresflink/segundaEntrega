2cfb8ed24c885396e3725563a170f17f48b237c2
andresflink <andresf.link@icloud.com>
2019-04-14 22:10:56 -0500

    no message

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,12 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.URLDB = 'mongodb://localhost:27017/matriculas';
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/asignaturas';
+}
+else {
+	urlDB = 'mongodb+srv://afarboledac:zelda1235!@nodejstdea-dbehn.mongodb.net/matriculas?retryWrites=true'
+}
+
+process.env.URLDB = urlDB
